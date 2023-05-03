0bdffce0f8285b815c40d4804d351f47969ae5c8
Daniyal Khan <emdaniyal24@gmail.com>
2023-05-03 13:27:35 +0500

    update 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-    return <h1>App</h1>;
+    const [comments, setComments] = useState([
+        { id: 1, text: "Comment 1" },
+        { id: 2, text: "Comment 2" },
+        { id: 3, text: "Comment 3" },
+    ]);
+
+    return (
+        <div className="container">
+            <h1>Hello, World!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dignissimos?</p>
+
+            {comments ? (
+                <>
+                    <h2>Comments ({comments.length})</h2>
+                    <ul>
+                        {comments.map((comment) => (
+                            <li key={comment.id}>{comment.text}</li>
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <p>No comments</p>
+            )}
+        </div>
+    );
 };
 
 export default App;
